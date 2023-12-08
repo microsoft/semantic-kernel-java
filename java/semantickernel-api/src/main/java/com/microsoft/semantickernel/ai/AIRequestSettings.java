f06f24097b169cc3bd2b22d1046d9a64763f2a11
David Grieve <dsgrieve@yahoo.com>
2023-12-08 10:25:34 -0500

    Java: Move sk-v1-proposal java source to semantic-kernel (#4008)
    ### TODO
    - [x] Update pom files. This may include disabling the build of existing
    modules.
    - [x] Get a clean compile. `./mvnw clean install -Prelease,with-samples
    -Dmaven.test.skip=true` works
    - [x] Where does the assistants package belong? ~~For now, I just put it
    in with 06-Assistants~~ Resolution: move to
    /plugins/semantickernel-plugin-core
    - [x] I'm not sure `aiservices-*` belong under `extensions` Resolution:
    make an aiservices module and put them there
    - [x] I'm not sure the bing plugin belongs under `plugins\web`
    Resolution: yeah, that works for now
    
    ### Motivation and Context
    
    <!-- Thank you for your contribution to the semantic-kernel repo!
    Please help reviewers and future users, providing the following
    information:
      1. Why is this change required?
      2. What problem does it solve?
      3. What scenario does it contribute to?
      4. If it fixes an open issue, please link to the issue here.
    -->
    We were working from two repos. This will make Java v1 development
    easier for all.
    
    ### Description
    
    <!-- Describe your changes, the overall approach, the underlying design.
    These notes will help understanding how your code works. Thanks! -->
    Took compiling code from sk-v1-proposal/java and moved it to
    semantic-kernel/java.
    Modified pom files as needed. 
    Modifications to code as needed to get it to compile. 
    
    ### Contribution Checklist
    
    <!-- Before submitting this PR, please make sure: -->
    
    - [ ] The code builds clean without any errors or warnings
    - [ ] The PR follows the [SK Contribution
    Guidelines](https://github.com/microsoft/semantic-kernel/blob/main/CONTRIBUTING.md)
    and the [pre-submission formatting
    script](https://github.com/microsoft/semantic-kernel/blob/main/CONTRIBUTING.md#development-scripts)
    raises no violations
    - [ ] All unit tests pass, and I have added new tests where possible
    - [ ] I didn't break anyone :smile:

--- java/semantickernel-api/src/main/java/com/microsoft/semantickernel/ai/AIRequestSettings.java
+++ java/semantickernel-api/src/main/java/com/microsoft/semantickernel/ai/AIRequestSettings.java
@@ -1,43 +1,10 @@
-// Copyright (c) Microsoft. All rights reserved.
 package com.microsoft.semantickernel.ai;
 
-import com.fasterxml.jackson.annotation.JsonCreator;
-import com.fasterxml.jackson.annotation.JsonProperty;
-import javax.annotation.Nullable;
+import java.util.Map;
 
-/** Request settings for an AI request. */
 public class AIRequestSettings {
-
-    /**
-     * Service identifier. This identifies a service and is set when the AI service is registered.
-     */
-    @JsonProperty("service_id")
-    @Nullable
-    private final String serviceId;
-
-    /// <summary>
-    /// Model identifier.
-    /// This identifies the AI model these settings are configured for e.g., gpt-4, gpt-3.5-turbo
-    /// </summary>
-    @JsonProperty("model_id")
-    @Nullable
-    private final String modelId;
-
-    @JsonCreator
-    public AIRequestSettings(
-            @JsonProperty("service_id") @Nullable String serviceId,
-            @JsonProperty("model_id") @Nullable String modelId) {
-        this.serviceId = serviceId;
-        this.modelId = modelId;
-    }
-
-    @Nullable
-    public String getServiceId() {
-        return serviceId;
-    }
-
-    @Nullable
-    public String getModelId() {
-        return modelId;
-    }
+    
+    public String serviceId() { return null; }
+    public String modelId() { return null; }
+    public Map<String, Object> extensionData() { return null; }
 }
